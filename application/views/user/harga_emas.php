55df2cd14f6829182129a7165686d2776ca9e6fc
Guntur Adi Prasetyo <gunturadiprasetyo6@gmail.com>
2023-11-26 20:12:43 +0800

    Harga Emas 26

--- application/views/user/harga_emas.php
+++ application/views/user/harga_emas.php
@@ -42,17 +42,19 @@ function tgl_indo($tanggal){
                     <center>
 
                         <h1><b> Harga Jual</b></h1>
-                        <h1 style="color:orange; font-size: 64px;">Rp. <?= number_format($harga['harga_jual'],0)?></h1>
+                        <h1 style="color:orange; font-size: 64px;">Rp. <?= number_format($harga['jual_6'],0)?></h1>
                         <br>
                         <!-- <br> -->
                         <h1><b> Harga Beli</b></h1>
-                        <h1 style="color:orange; font-size: 64px;">Rp. <?= number_format($harga['harga_beli'],0)?></h1>
+                        <h1 style="color:orange; font-size: 64px;">Rp. <?= number_format($harga['beli_6'],0)?></h1>
 
                         <!-- <br> -->
 
                     </center>
                     <div class="text-center">
-                    <a href="https://api.whatsapp.com/send?phone=6281937608593&text=Halo%20Wahyu%20Redjo">Beli Sekarang</a>
+                        <a href="https://api.whatsapp.com/send?phone=6281937608593&text=Halo%20Wahyu%20Redjo">Beli
+                            Sekarang</a>
+
                     </div>
                 </div>
             </div>
@@ -61,6 +63,101 @@ function tgl_indo($tanggal){
                     <img src="<?= base_url()?>assets/images/about-img.png" alt="">
                 </div>
             </div>
+        </div>
+    </div>
+</section>
+<section class="ring_section layout_padding">
+    <div class="design-box">
+        <img src="images/design-1.png" alt="">
+    </div>
+    <div class="container">
+        <div class="ring_container layout_padding2">
+            <h2>Detail Harga Emas Hari Ini </h2>
+            <span>Update Tanggal <?= tgl_indo(date('Y-m-d',$harga['time']))?></span>
+            <div class="row">
+
+                <div class="col-md-5">
+                    <div class="detail-box">
+                        <table class="table table-bordered" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Kadar</th>
+                                    <th>Harga Jual</th>
+                                    <th>Harga Beli</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>6K</td>
+                                    <td>Rp. <?= number_format($harga['jual_6'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_6'],0)?></td>
+                                </tr>
+                                <tr>
+                                    <td>8K / 9K</td>
+                                    <td>Rp. <?= number_format($harga['jual_8'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_8'],0)?></td>
+                                </tr>
+                                <tr>
+                                    <td>16K</td>
+                                    <td>Rp. <?= number_format($harga['jual_16'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_16'],0)?></td>
+                                </tr>
+                                <tr>
+                                    <td>1K</td>
+                                    <td>Rp. <?= number_format($harga['jual_17'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_17'],0)?></td>
+                                </tr>
+                                <tr>
+                                    <td>8K / 9K P</td>
+                                    <td>Rp. <?= number_format($harga['jual_8_p'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_8_p'],0)?></td>
+                                </tr>
+                                <tr>
+                                    <td>24K / HK</td>
+                                    <td>Rp. <?= number_format($harga['jual_24'],0)?></td>
+                                    <td>Rp. <?= number_format($harga['beli_24'],0)?></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="img-box">
+                        <img src="<?= base_url()?>assets/images/ring-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="price_section layout_padding">
+    <div class="container">
+        <div class="heading_container">
+            <h2>
+                Rincian Harga Emas Hari Ini
+            </h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Gram</th>
+                        <th>Harga Jual</th>
+                        <th>Harga Beli</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="price_container">
+
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="" class="price_btn">
+                See More
+            </a>
         </div>
     </div>
 </section>
--- wahyu_redjo.sql
+++ wahyu_redjo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Nov 2023 pada 16.35
+-- Waktu pembuatan: 25 Nov 2023 pada 16.13
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.1
 
@@ -30,8 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `harga_emas` (
   `id` int(11) NOT NULL,
-  `harga_jual` int(11) NOT NULL,
-  `harga_beli` int(11) NOT NULL,
+  `jual_6` int(11) NOT NULL,
+  `beli_6` int(11) NOT NULL,
+  `jual_8` int(11) NOT NULL,
+  `beli_8` int(11) NOT NULL,
+  `jual_16` int(11) NOT NULL,
+  `beli_16` int(11) NOT NULL,
+  `jual_17` int(11) NOT NULL,
+  `beli_17` int(11) NOT NULL,
+  `jual_8_p` int(11) NOT NULL,
+  `beli_8_p` int(11) NOT NULL,
+  `jual_24` int(11) NOT NULL,
+  `beli_24` int(11) NOT NULL,
   `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -39,8 +49,8 @@ CREATE TABLE `harga_emas` (
 -- Dumping data untuk tabel `harga_emas`
 --
 
-INSERT INTO `harga_emas` (`id`, `harga_jual`, `harga_beli`, `time`) VALUES
-(1, 9000044, 900001, 1700806508);
+INSERT INTO `harga_emas` (`id`, `jual_6`, `beli_6`, `jual_8`, `beli_8`, `jual_16`, `beli_16`, `jual_17`, `beli_17`, `jual_8_p`, `beli_8_p`, `jual_24`, `beli_24`, `time`) VALUES
+(1, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1700923801);
 
 -- --------------------------------------------------------
 
@@ -63,6 +73,39 @@ CREATE TABLE `outlet` (
 INSERT INTO `outlet` (`id_outlet`, `kota`, `alamat`, `no_tlp`, `foto`) VALUES
 (5, 'Malang', 'Sukun', 2147483647, 'Malang.JPG'),
 (6, 'Sidoarjo', 'Gaeor', 2147483647, 'Sidoarjo.JPG');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `riwayat_harga_emas`
+--
+
+CREATE TABLE `riwayat_harga_emas` (
+  `id_riwayat` int(11) NOT NULL,
+  `jual_6` int(11) NOT NULL,
+  `beli_6` int(11) NOT NULL,
+  `jual_8` int(11) NOT NULL,
+  `beli_8` int(11) NOT NULL,
+  `jual_16` int(11) NOT NULL,
+  `beli_16` int(11) NOT NULL,
+  `jual_17` int(11) NOT NULL,
+  `beli_17` int(11) NOT NULL,
+  `jual_8_p` int(11) NOT NULL,
+  `beli_8_p` int(11) NOT NULL,
+  `jual_24` int(11) NOT NULL,
+  `beli_24` int(11) NOT NULL,
+  `tgl` int(11) NOT NULL,
+  `bln` int(11) NOT NULL,
+  `thn` int(11) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `riwayat_harga_emas`
+--
+
+INSERT INTO `riwayat_harga_emas` (`id_riwayat`, `jual_6`, `beli_6`, `jual_8`, `beli_8`, `jual_16`, `beli_16`, `jual_17`, `beli_17`, `jual_8_p`, `beli_8_p`, `jual_24`, `beli_24`, `tgl`, `bln`, `thn`, `time`) VALUES
+(2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 11, 2023, 1700923801);
 
 -- --------------------------------------------------------
 
@@ -100,6 +143,12 @@ ALTER TABLE `outlet`
   ADD PRIMARY KEY (`id_outlet`);
 
 --
+-- Indeks untuk tabel `riwayat_harga_emas`
+--
+ALTER TABLE `riwayat_harga_emas`
+  ADD PRIMARY KEY (`id_riwayat`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -120,6 +169,12 @@ ALTER TABLE `harga_emas`
 --
 ALTER TABLE `outlet`
   MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `riwayat_harga_emas`
+--
+ALTER TABLE `riwayat_harga_emas`
+  MODIFY `id_riwayat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
